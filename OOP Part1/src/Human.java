92bd9051dcd8ee0279b3b9681050936d4a2ef576
Stikender <nadik17@bk.ru>
2022-12-15 00:51:24 +0800

    Exercise 1.1,1.2

--- OOP Part1/src/Human.java
+++ OOP Part1/src/Human.java
@@ -2,11 +2,13 @@ public class Human {
   public int yearOfBirth;
   public String name;
   public String town;
+    String jobTitle;
 
-  public Human(int yearOfBirth, String name, String town) {
+  public Human(int yearOfBirth, String name, String town, String jobTitle) {
       this.yearOfBirth = yearOfBirth;
       this.name = name;
       this.town = town;
+      this.jobTitle = jobTitle;
   }
 
     @Override
@@ -14,7 +16,8 @@ public class Human {
         return "Привет! Меня зовут " + name +
                 ". Я из города " + town +
                 ". Я родился в " + yearOfBirth +
-                " году. Будем знакомы!";
+                " году. Я работаю на должности " + jobTitle +
+                ". Будем знакомы!";
     }
 
 }
