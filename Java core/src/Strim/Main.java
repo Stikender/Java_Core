54093ad4c4d6244422ada1d1cbc107943f2a6c0b
Stikender <nadik17@bk.ru>
2023-02-10 00:36:06 +0800

    Stream Finish commit

--- Java core/src/Strim/Main.java
+++ Java core/src/Strim/Main.java
@@ -11,15 +11,16 @@ public class Main {
     public static void main(String[] args) {
 //  Задание 1
         List<Integer> list = new ArrayList<>();
-        list.add(3);
+        list.add(1);
         list.add(5);
         list.add(7);
         list.add(2);
         list.add(4);
         list.add(8);
         Stream stream = list.stream();
+
         findMinMax(stream, Integer::compareTo,
-                (min, max) -> System.out.println("Минимальное число - " + min + ", Максимальное число - " + max));
+                (min, max) -> stream.toString());
 
 //  Задание 2
 
@@ -32,10 +33,15 @@ public class Main {
         List<? extends T> list = stream
                 .sorted(comparator)
                 .collect(Collectors.toList());
-        minMaxConsumer.accept(list.get(0), list.get(list.size() - 1));
+        if (list.isEmpty()) {
+            minMaxConsumer.accept(null, null);
+            System.out.println("Минимальное число - " + null + ", Максимальное число - " + null);
+        } else {
+            minMaxConsumer.accept(list.get(0), list.get(list.size() - 1));
+            System.out.println("Минимальное число - " + list.get(0) + ", Максимальное число - " + list.get(list.size()-1));
+        }
 
     }
-
     public static void numberEven (List<Integer> list) {
         long count = list.stream()
                 .filter(i -> i % 2 == 0)
